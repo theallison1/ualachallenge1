d795d7831469babd78508ac334a18ac9cfcb9947
nicolas espinosa <nico.2011.pr@gmail.com>
2024-06-19 23:29:46 -0300

    H2

--- src/main/java/com/challenge/uala/model/Usuarios.java
+++ src/main/java/com/challenge/uala/model/Usuarios.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "USUARIOS")
 public class Usuarios {
@@ -13,22 +17,7 @@ public class Usuarios {
     @GeneratedValue
     private Long id;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     private String name;
 
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
