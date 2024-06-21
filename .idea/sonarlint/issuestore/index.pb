d797e159a9731ee0775bcda4f115cdda8c98bce4
nicolas espinosa <nico.2011.pr@gmail.com>
2024-06-21 03:00:21 -0300

    clase usuarios

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 d
 4src/main/java/com/challenge/uala/model/Usuarios.java,6/2/62259bc642f9363908314af292dfa422ad13f32c
+8
+uala.iml,e/c/ec2442e33b04418bf0a5c98472ac6a34d2015c9d
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/com/challenge/uala/model/Usuarios.java
+++ src/main/java/com/challenge/uala/model/Usuarios.java
@@ -4,20 +4,33 @@ package com.challenge.uala.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-@Getter
-@Setter
+import java.time.LocalDate;
+
+
+@Data
 @Entity
-@Table(name = "USUARIOS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuarios {
+
     @Id
     @GeneratedValue
     private Long id;
-
+    @NotNull
     private String name;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private LocalDate birthDate;
+    @NotNull
+    private String email;
+    @NotNull
+    private String phone;
+
 
 
 }
